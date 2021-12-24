7a98419f439cadac2d391ab1957cdb3d6ad2d5ef
N0xLevx <n0xmare@levxdao.org>
2021-12-24 12:04:26 -0500

    bruh

--- src/Events/messageCreate.js
+++ src/Events/messageCreate.js
@@ -14,29 +14,15 @@ module.exports = new Event("messageCreate", async (client, message) => {
 
         command.run(message, args, client);
 
-    } else if (message.channel.isThread()) { //For a fight
+    } else if (message.channel.isThread()) { //Fight
 
         let player = message.author.username
         let players = message.channel.name.split(' -vs- ')
 
-        //checks if they are a player. If the game uses emoji reactions you could have a similar
-        //thing for that event
+    
         if(player != players[0] || player != players[1]) await message.delete()
     
         //Fight actions
 
     }
-
-
-	/*
-	if (!message.content.startsWith(client.prefix)) return;
-
-	const args = message.content.substring(client.prefix.length).split(/ +/);
-
-	const command = client.commands.find(cmd => cmd.name === args[0]);
-
-	if (!command) return message.reply(`${args[0]} is not a valid command!`);
-
-	command.run(message, args, client);
-	*/
 });
